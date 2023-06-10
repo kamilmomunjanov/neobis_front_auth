c4a0fd0ffd6a68a0563e45bcf193fb9f13c94faf
Kamil <kamilmomunjanov@gmail.com>
2023-06-11 00:11:05 +0600

    home layout

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React, {useContext} from 'react';
 import {CustomContext} from "../../config/context/context";
+import "./Home.css";
 
 const Home = () => {
     const {user} = useContext(CustomContext)
     return (
-        <div>
+        <div className="home">
             Поздравляем вы вошли в аккаунт
             Email : {user.email}
         </div>
